7edf9fdb110c83838cde080dfecfd9d9682305c1
Rashitko <michal.raska@gmail.com>
2013-11-14 17:21:30 +0100

    vytvorená kostra aplikácie

---
+++ skladisko/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skladisko::Application.config.secret_key_base = 'f983a5e9fa97dbed06c777cd8ae89b4df0d407ff1624cf838f8454b0f34681fd8ebf279ccb1669ef0235894c2b2f53d751041cd7e4def4c60f71e6a7004156f9'
