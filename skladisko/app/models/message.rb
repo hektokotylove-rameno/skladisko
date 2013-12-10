0fc51c7be54a52d9f017b400a2ead8ff85164fe6
Rashitko <michal.raska@gmail.com>
2013-12-10 18:09:11 +0100

    Jemne upravy listovania sprav

--- skladisko/app/models/message.rb
+++ skladisko/app/models/message.rb
@@ -2,4 +2,12 @@ class Message < ActiveRecord::Base
   
   belongs_to :chemical
   
+  #def self.test
+  #  message = Message.new
+  #  chem = Chemical.find_by_id(1)
+  #  message.chemical = chem
+  #  message.text = 'test message'
+  #  message.save
+  #end
+  
 end
