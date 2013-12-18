015c87a96275f67b4fff5330387844bdbb065b40
Rashitko <michal.raska@gmail.com>
2013-12-18 18:14:41 +0100

    Upravene zobrazovanie settingov, buttony na dump a load, zatial nefunkcne!

--- skladisko/app/controllers/settings_controller.rb
+++ skladisko/app/controllers/settings_controller.rb
@@ -3,13 +3,46 @@ class SettingsController < ApplicationController
     @setting = Setting.find(1)
     render 'index'
     session[:message] = nil
+    session[:type] = nil
   end
   
   def save
+    if (params[:save])
+      save_database
+    end
+    if (params[:load])
+      if File.exists?('./db/data.yml')
+        
+      end
+      
+    end
+    if (params[:settings])
+      save_settings
+    end
+  end
+  
+  def save_database
+    system "/usr/local/bin/rake db:data:dump >> ./logs/db_dump.txt"
+    if File.exists?('./logs/db_dump.txt')
+      session[:message] = 'Error occured while saving database'
+      session[:type] = 'error'
+      File.delete('./logs/db_dump.txt')
+    else
+      session[:message] = 'Database saved'
+    end
+    redirect_to settings_path
+  end
+  
+  def save_settings
     @setting = Setting.find(1)
     @setting.days_before_warn = params[:setting][:days_before_warn]
     @setting.save
     session[:message] = 'Settings saved'
+    redirect_to settings_path
+  end
+  
+  def dump
+    session[:message] = 'Database saved successfully'
     redirect_to settings_path
   end
   
--- skladisko/db/seeds.rb
+++ skladisko/db/seeds.rb
@@ -1,18 +1,1 @@
-Chemical.create!([
-  {name: "Voda", total_amount: 0.0, critical_amount: 0.0, unit: "l", group_id: 1, note: ""},
-  {name: "Pivo", total_amount: 0.0, critical_amount: 0.0, unit: "l", group_id: 1, note: ""},
-  {name: "Krv", total_amount: 0.0, critical_amount: 0.0, unit: "ml", group_id: 1, note: ""},
-  {name: "Pečené kura", total_amount: 0.0, critical_amount: 0.0, unit: "g", group_id: 2, note: ""},
-  {name: "Hovno", total_amount: 0.0, critical_amount: 0.0, unit: "kg", group_id: 3, note: ""}
-])
-Group.create!([
-  {name: "tekutiny"},
-  {name: "chutné látky"},
-  {name: "smradľavé látky"}
-])
-Setting.create!([
-  {days_before_warn: 0}
-])
-User.create!([
-  {username: "admin", name: "Admin", password_hash: "$2a$10$IBmuU2sGDduXZlQrYicbbui13CvbXWL.tg2KM24IV5TWIs1N0xY2u", password_salt: "$2a$10$IBmuU2sGDduXZlQrYicbbu", admin: nil}
-])
+Setting.create()
