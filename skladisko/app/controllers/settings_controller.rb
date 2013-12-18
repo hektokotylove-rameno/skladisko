ae70d750ab6533e747aca85c215de003e462dbb4
Ainer <horvath.adam91@gmail.com>
2013-12-18 18:15:10 +0100

    validation message

--- skladisko/app/controllers/settings_controller.rb
+++ skladisko/app/controllers/settings_controller.rb
@@ -3,14 +3,20 @@ class SettingsController < ApplicationController
     @setting = Setting.find(1)
     render 'index'
     session[:message] = nil
+    
   end
   
   def save
     @setting = Setting.find(1)
     @setting.days_before_warn = params[:setting][:days_before_warn]
-    @setting.save
-    session[:message] = 'Settings saved'
-    redirect_to settings_path
+    if(@setting.save)
+      session[:message] = 'Settings saved'
+      redirect_to settings_path
+    else
+      session[:type] = 'Error'
+      session[:message] = 'Must be a number'
+      redirect_to settings_path
+    end
   end
   
 end
