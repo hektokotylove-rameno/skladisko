d52549a45b210808c46008835dcf9304ef36da49
Ainer <horvath.adam91@gmail.com>
2013-12-20 10:23:50 +0100

    encoding do seedu + drop_and_seed.bat pre win

--- skladisko/db/seeds.rb
+++ skladisko/db/seeds.rb
@@ -1,3 +1,5 @@
+#encoding: utf-8
+
 Chemical.create!([
   {name: "Hovno", total_amount: 0.0, critical_amount: 1.0, unit: "kg", group_id: 1, note: "hovnisko"},
   {name: "Pivo", total_amount: 0.0, critical_amount: 1.5, unit: "l", group_id: 2, note: "pivcisko"},
