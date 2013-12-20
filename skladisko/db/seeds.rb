d5b30852c3f22473bf15cca14d5b4bfeff4dd1e6
Ainer <horvath.adam91@gmail.com>
2013-12-20 10:27:15 +0100

    working user pridany do seedu

--- skladisko/db/seeds.rb
+++ skladisko/db/seeds.rb
@@ -1,5 +1,7 @@
 #encoding: utf-8
 
+
+
 Chemical.create!([
   {name: "Hovno", total_amount: 0.0, critical_amount: 1.0, unit: "kg", group_id: 1, note: "hovnisko"},
   {name: "Pivo", total_amount: 0.0, critical_amount: 1.5, unit: "l", group_id: 2, note: "pivcisko"},
@@ -13,4 +15,9 @@ Group.create!([
 ])
 Setting.create!([
   {days_before_warn: 0}
+])
+
+User.create!([
+  {username: "admin", name: "Admin", password_hash: "$2a$10$eZlnTjkyqLhpiNOOamg2sOmR5khIK.zXgjeo1W3r.icQ/Xu7cgom2", password_salt: "$2a$10$eZlnTjkyqLhpiNOOamg2sO", admin: true, new_message: false},
+  {username: "timo", name: "Timotej Krajči", password_hash: "$2a$10$DKgE0XhSbezBBAOM4a03PeWslP6N7OfzK5ynPG97eejqSJ/i/T3Ji", password_salt: "$2a$10$DKgE0XhSbezBBAOM4a03Pe", admin: nil, new_message: false}
 ])
