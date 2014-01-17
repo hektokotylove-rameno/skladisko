a558a500aa3bb73046a84816bc92e100e602f9c7
Rashitko <michal.raska@gmail.com>
2014-01-17 14:14:03 +0100

    Pridany filter na koncovky uploadovaneho suboru, omarginovany button pri submite attachmentu

--- skladisko/app/uploaders/attachment_uploader.rb
+++ skladisko/app/uploaders/attachment_uploader.rb
@@ -36,11 +36,11 @@ class AttachmentUploader < CarrierWave::Uploader::Base
   #   process :scale => [50, 50]
   # end
 
-  # Add a white list of extensions which are allowed to be uploaded.
-  # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+   #Add a white list of extensions which are allowed to be uploaded.
+   #For images you might use something like this:
+   def extension_white_list
+     %w(yml)
+   end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
